8f95587201e2b0943436d618d7d883392bcf54d3
vadimdovnar <84572478+vadimdovnar@users.noreply.github.com>
2022-04-21 18:45:35 +0300

    create layout and layoutItems

--- force-app/main/default/aura/BorderPanel/BorderPanelController.js
+++ force-app/main/default/aura/BorderPanel/BorderPanelController.js
@@ -1,5 +1,11 @@
 ({
-    myAction : function(component, event, helper) {
+    startGame : function(component, event, helper) {
 
+        const selectedGameMode = component.find("SelectedGame");
+        let selectedValue = selectedGameMode.get("v.value");
+        component.set("v.SelectedMode", selectedValue);
+    },
+    reshuffle : function(component, event, helper) {
+        console.log('Reshuffle');
     }
 })
